c5d56a540f588816c61a351dc8e86e05ed5ba54d
danielluzgarten <danielu3025dl>
2017-01-08 22:34:20 +0200

    כל הגיט הזה פח

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,15 @@ public class Main{
         connect();
         if(con != null) {
             //ClassRoom g = new ClassRoom(con);
-            Lecturer l = new Lecturer(con);
+            //Lecturer l = new Lecturer(con);
+            Cours c = new Cours(con);
         }
     }
     private static void connect() throws Exception {
         try {
             String driver = "com.mysql.jdbc.Driver";
-            //String url = "jdbc:mysql://localhost:8889/schedulerDB";
-            String url = "jdbc:mysql://localhost:3306/schedulerDB";
+            String url = "jdbc:mysql://localhost:8889/schedulerDB";
+            //String url = "jdbc:mysql://localhost:3306/schedulerDB";
             String userName = "root";
             String password = "root";
             Class.forName(driver);
